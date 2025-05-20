0aa9a7ff95befb4b94662c81b6b189aef3cb2f98
Cauê <caue@mail.com>
2025-05-20 20:26:01 -0300

    Criação da classe LivroController na pasta de controller.

--- src/main/java/com/biblioteca/repository/LivroRepository.java
+++ src/main/java/com/biblioteca/repository/LivroRepository.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.biblioteca.repository;
 
-/**
- *
- * @author Cauê
- */
-public class LivroRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.biblioteca.model.Livro;
+
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
     
 }
