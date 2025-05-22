28d3de271b040533e45f1e37582627858c1f9954
Cauê Lopes Justo - 82329174 <82329174@ulife.com.br>
2025-05-22 10:20:00 -0300

    Criado interface das entidades Usuario e Emprestimo na pasta repository.

---
+++ src/main/java/com/biblioteca/repository/EmprestimoRepository.java
@@ -0,0 +1,9 @@
+
+package com.biblioteca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.biblioteca.model.Emprestimo;
+
+public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer> {
+    
+}
