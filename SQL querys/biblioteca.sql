f0dc7fb221034e5b9200ed8dbc4bc3322d802a71
Cauê <caue@mail.com>
2025-06-23 14:27:25 -0300

    Add: txt para funcionalidades futuras para a aplicação.

--- SQL querys/biblioteca.sql
+++ SQL querys/biblioteca.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS usuario (
     telefone VARCHAR(20),
     email VARCHAR(50),
     senha VARCHAR(20) not null,
-    registro int not null
+    registro varchar(20) not null
 );
 
 CREATE TABLE IF NOT EXISTS leitor (
@@ -57,5 +57,7 @@ INSERT INTO Livro (Titulo, ano, num_paginas, isbn, editora, genero, autor) VALUE
 INSERT INTO usuario (nome, endereco, telefone, email, registro, senha) VALUES ('ADM', 'Rua Y', '(11) 123456789', 'danilo@email.com', 'adm', 'adm');
 INSERT INTO leitor (nome, endereco, telefone, email, registro) VALUES ('Leitor', 'Rua Y', '(11) 123456789', 'leitor@email.com', '12345');
 INSERT INTO emprestimo (id_leitor, id_livro, data_emprestimo, data_devolucao, devolvido) VALUES (1, 3, "2025-05-01", null , FALSE);
+
+DELETE FROM leitor WHERE registro = 1596;
 
 #Realizar o o que, o porque e o como do projeto
